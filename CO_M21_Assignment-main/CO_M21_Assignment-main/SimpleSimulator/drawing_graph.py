6e179b67343583c914c106253667ec7bd5517967
SarthakTyagi20 <85821901+SarthakTyagi20@users.noreply.github.com>
2021-08-27 17:36:15 +0530

    Update drawing_graph.py

--- CO_M21_Assignment-main/CO_M21_Assignment-main/SimpleSimulator/drawing_graph.py
+++ CO_M21_Assignment-main/CO_M21_Assignment-main/SimpleSimulator/drawing_graph.py
@@ -1,1 +1,19 @@
-import sys
+import matplotlib.pyplot as plt
+from sys import stdin
+i = 1
+x = []
+y = []
+with open('/mnt/d/Sem_2/CO/CO_M21_Assignment-main-finalfinal/SimpleSimulator/D:\Sem_2\CO\CO_M21_Assignment-main-finalfinal\SimpleSimulator\graph.txt') as f:
+    line = f.readline()
+    while line:
+        
+        y.append(line)
+        x.append(i)
+        i = i + 1
+        line = f.readline()
+ 
+plt.scatter(x, y)
+plt.title("Scatter plot")
+plt.xlabel("Cycle Number")
+plt.ylabel("Memory Address")
+plt.show()
